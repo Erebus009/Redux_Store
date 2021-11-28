7b05d4264f36f49fbcb48dc8c9c7f1f4929cf0c1
Travis Puryear <travispuryear678@gmail.com>
2021-11-28 15:15:18 -0500

    Add Provider to app.js and remove globalstate import.

--- client/src/App.js
+++ client/src/App.js
@@ -14,10 +14,10 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
-
+import { Provider } from 'react';
+import onlineStore from './utils/onlineStore';
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
@@ -42,7 +42,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider onlineStore = {onlineStore}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -53,7 +53,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
